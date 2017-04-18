56a51b2c51a3dec0f3989098e8a8639ad65c4546
Tahmid Shakil <md.ahnaf.tahmid.shakil@framgia.com>
2017-04-18 16:23:14 +0600

    Clean up

---
+++ app/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  Image, ScrollView, Button
+} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Text22222221
+        </Text>
+      </View>
+    );
+  }
+}
