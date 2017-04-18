4d0e180057b251a18ad6486de8b54e08f3f33f9e
Tahmid Shakil <md.ahnaf.tahmid.shakil@framgia.com>
2017-04-18 16:48:51 +0600

    Deep linking basics

--- app/app.js
+++ app/app.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import {
-  Text,
-  View,
-  Image, ScrollView, Button
-} from 'react-native';
+import { Text, View, Image, ScrollView, Button, Platform } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View>
-        <Text>
-          Text22222221
-        </Text>
-      </View>
-    );
+const S1 = () => <View><Text>S1 text</Text></View>;
+const S2 = () => <View><Text>S2 text</Text></View>;
+const S3 = () => <View><Text>S3 text</Text></View>;
+
+export default StackNavigator(
+  {
+    S1: {screen: S1},
+    S2: {screen: S2, path: 's2'},
+    S3: {screen: S3, path: 's3'}
+  },
+  {
+    containerOptions: {
+      URIPrefix: Platform.OS == 'android' ? 'http://localhost/' : 'http://'
+    }
   }
-}
+);
