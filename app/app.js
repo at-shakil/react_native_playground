19fb49db51a70ac2aa02954303e8c0b95ad56f2c
Tahmid Shakil <md.ahnaf.tahmid.shakil@framgia.com>
2017-04-18 17:07:30 +0600

    Redux integration phase 1

--- app/app.js
+++ app/app.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import { Text, View, Image, ScrollView, Button, Platform } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import { Provider, connect } from 'react-redux';
 
 const S1 = () => <View><Text>S1 text</Text></View>;
 const S2 = () => <View><Text>S2 text</Text></View>;
 const S3 = () => <View><Text>S3 text</Text></View>;
 
-export default StackNavigator(
+const AppNav = StackNavigator(
   {
     S1: {screen: S1},
     S2: {screen: S2, path: 's2'},
     S3: {screen: S3, path: 's3'}
+  }
+);
+
+const CoreNav = StackNavigator(
+  {
+    App: {screen: AppNav}
   },
   {
     containerOptions: {
@@ -18,3 +25,5 @@ export default StackNavigator(
     }
   }
 );
+
+export default CoreNav;
