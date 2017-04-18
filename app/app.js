5ce99cd37b7ff32d24832a2d4d2a862e7e8326e7
Tahmid Shakil <md.ahnaf.tahmid.shakil@framgia.com>
2017-04-18 18:53:09 +0600

    Deep linking nested routes

--- app/app.js
+++ app/app.js
@@ -6,11 +6,22 @@ const S1 = () => <View><Text>S1 text</Text></View>;
 const S2 = () => <View><Text>S2 text</Text></View>;
 const S3 = () => <View><Text>S3 text</Text></View>;
 
-export default StackNavigator(
+const S5 = () => <View><Text>S5 text</Text></View>;
+const S6 = () => <View><Text>S6 text</Text></View>;
+
+const Nav1 = StackNavigator(
+  {
+    S5: {screen: S5, path: 's5'},
+    S6: {screen: S6, path: 's6'}
+  }
+);
+
+const Nav2 = StackNavigator(
   {
     S1: {screen: S1},
     S2: {screen: S2, path: 's2'},
-    S3: {screen: S3, path: 's3'}
+    S3: {screen: S3, path: 's3'},
+    S4: {screen: Nav1, path: 's4'}
   },
   {
     containerOptions: {
@@ -18,3 +29,5 @@ export default StackNavigator(
     }
   }
 );
+
+export default Nav2;
