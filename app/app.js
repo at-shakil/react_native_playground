473f9e28fde194e321ddf8aec0b8258c32ef1200
Tahmid Shakil <md.ahnaf.tahmid.shakil@framgia.com>
2017-04-21 19:02:03 +0600

    Custom navigator integration

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Text, View, Image, ScrollView, Button, Platform } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { StackNavigator, StackRouter, addNavigationHelpers } from 'react-navigation';
+
+// URIPrefix: Platform.OS == 'android' ? 'http://localhost/' : 'http://'
 
 const S1 = () => <View><Text>S1 text</Text></View>;
 const S2 = () => <View><Text>S2 text</Text></View>;
@@ -9,12 +11,34 @@ const S3 = () => <View><Text>S3 text</Text></View>;
 const S5 = () => <View><Text>S5 text</Text></View>;
 const S6 = () => <View><Text>S6 text</Text></View>;
 
-const Nav1 = StackNavigator(
-  {
+// const Nav1 = StackNavigator(
+//   {
+//     S5: {screen: S5, path: 's5'},
+//     S6: {screen: S6, path: 's6'}
+//   }
+// );
+
+class Nav1 extends Component {
+  static router = StackRouter({
     S5: {screen: S5, path: 's5'},
     S6: {screen: S6, path: 's6'}
+  });
+
+  render() {
+    const
+      { navigation } = this.props,
+      { state, dispatch } = navigation,
+      { routes, index } = state,
+      Component = router.getComponentForState(state);
+
+    let
+      childNavigation = { dispatch, state: routes[index] };
+
+    childNavigation = addNavigationHelpers(childNavigation);
+
+    return <Component navigation={childNavigation} />;
   }
-);
+}
 
 const Nav2 = StackNavigator(
   {
@@ -22,11 +46,6 @@ const Nav2 = StackNavigator(
     S2: {screen: S2, path: 's2'},
     S3: {screen: S3, path: 's3'},
     S4: {screen: Nav1, path: 's4'}
-  },
-  {
-    containerOptions: {
-      URIPrefix: Platform.OS == 'android' ? 'http://localhost/' : 'http://'
-    }
   }
 );
 
