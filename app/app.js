8184c289e93b0fa54f318b86fe037c57f38519d7
Tahmid Shakil <md.ahnaf.tahmid.shakil@framgia.com>
2017-04-24 20:46:10 +0600

    Redux integration phase 5

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { createStore, combineReducers } from 'redux';
-import { Text, View, Image, ScrollView, Button, Platform } from 'react-native';
+import { Text, View, Image, ScrollView, Button, Platform, Linking } from 'react-native';
 import { StackNavigator, StackRouter, addNavigationHelpers } from 'react-navigation';
 import { Provider, connect } from 'react-redux';
 
@@ -8,13 +8,71 @@ const S1 = () => <View><Text>S1 text</Text></View>;
 const S2 = () => <View><Text>S2 text</Text></View>;
 const S3 = () => <View><Text>S3 text</Text></View>;
 
-const AppNav = StackNavigator(
-  {
-    S1: {screen: S1, path: 's1'},
-    S2: {screen: S2, path: 's2'},
-    S3: {screen: S3, path: 's3'}
+class AppNav extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.handleUrl = this.handleUrl.bind(this);
   }
-);
+
+  static router = StackRouter(
+    {
+      S1: {screen: S1, path: 's1'},
+      S2: {screen: S2, path: 's2'},
+      S3: {screen: S3, path: 's3'}
+    },
+    {
+
+    }
+  );
+
+  handleUrl(url) {
+    console.warn(url);
+    const
+      { navigation } = this.props,
+      { state, dispatch } = navigation,
+      uriPrefix = "http://localhost",
+      params = {},
+      Component = AppNav.router.getComponentForState(state);
+    let
+      path = url.split(uriPrefix)[1];
+    if(!path) {
+      path = url;
+    }
+
+    console.warn("Path: "+path+", Params: "+JSON.stringify(params));
+
+    const action = AppNav.router.getActionForPathAndParams(path, params);
+
+    if(action) {
+      dispatch(action);
+    }
+  }
+
+  componentDidMount() {
+    Linking.addEventListener('url', ({ url }: {url: string}) => {
+      this.handleUrl(url);
+    });
+
+    Linking.getInitialURL().then(
+      (url: string) => url && this.handleUrl(url)
+    );
+  }
+
+  render() {
+    const
+      { navigation } = this.props,
+      { state, dispatch } = navigation,
+      { routes, index } = state,
+      Component = AppNav.router.getComponentForState(state);
+    let
+      childNavigation = { dispatch, state: routes[index] };
+    childNavigation = addNavigationHelpers(childNavigation);
+
+    return (
+      <Component navigation={childNavigation} />
+    );
+  }
+}
 
 @connect(state => ({nav: state.nav}))
 class App extends Component {
@@ -26,7 +84,6 @@ class App extends Component {
     return (
       <AppNav
         navigation={addNavigationHelpers({dispatch: this.props.dispatch, state: this.props.nav})}
-        //uriPrefix={uriPrefix}
       />
     );
   }
